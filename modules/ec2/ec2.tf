73a2ad2475c609be3a0a4e6a593c9e38ed899394
abhay_bajeta <abhaybajeta433@gmail.com>
2022-12-27 14:24:34 +0530

    initial commit

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -5,7 +5,6 @@ resource "aws_key_pair" "terra-key" {
 
 resource "aws_instance" "aesthisia-ec2" {
   ami = var.AMI
-  region = var.REGION
   instance_type = "t2.micro"
   availability_zone = var.ZONE1
   key_name = aws_key_pair.terra-key.key_name
