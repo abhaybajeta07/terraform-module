937675c7dddf6854dbe7ec50d86c7d5780525c5b
abhay_bajeta <abhaybajeta433@gmail.com>
2022-12-29 11:30:06 +0530

    eks

--- EKS/worker-node.tf
+++ EKS/worker-node.tf
@@ -2,7 +2,7 @@ resource "aws_eks_node_group" "worker-node" {
   cluster_name    = aws_eks_cluster.aesthisia-cluster.name
   node_group_name = "eks-node-group"
   node_role_arn   = aws_iam_role.worker-node-role.arn
-  subnet_ids      = [aws_subnet.private_subnet1.id, aws_subnet.private_subnet2.id, aws_subnet.private_subnet3.id, aws_subnet.public_subnet1.id, aws_subnet.public_subnet2.id, aws_subnet.public_subnet3.id]
+  subnet_ids      = [aws_subnet.private_subnet1.id, aws_subnet.private_subnet2.id, aws_subnet.private_subnet3.id]
 
   scaling_config {
     desired_size = 3
